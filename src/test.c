91597792a7520e48cf41a1f0cb1022729b738e03
Chengyu Zheng <chengyu.zheng@polimi.it>
2017-10-09 16:49:07 +0200

    c / objdump example

--- src/test.c
+++ src/test.c
@@ -1,21 +1,40 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-
-int  __attribute__((stdcall)) sum(int a, int b){
+int __attribute__((cdecl)) sum1(int a, int b){
     return a+b;
 }
 
+int __attribute__((stdcall)) sum2(int a, int b){
+    return a+b;
+}
+
+int __attribute__((fastcall)) sum3(int a, int b){
+    return a+b;
+}
 
 int  main(int argc, char *argv[]){
+    // calling convention
+    int a = 2;
+    int b = 3;
 
-    int a = 3;
-    int b = 5;
-    int c = 7;
+    sum1(a,b);
+    sum2(a,b);
+    sum3(a,b);
 
-    printf("%d + %d = %d\n",a,b, sum(a,b));
+    // endianess
+    int c = 0x41424344;
+    printf("%x\n",c);
 
-    printf("%d + %d = %d\n",c,b, sum(c,b));
+    // syscall
+    int fd = open("test.c", O_RDONLY);
+    char buf[10];
+    read(fd, buf, 8);
+    write(0, buf, 8);
+    write(0, "\n", 1);
+    close(fd);
 
-
+    return 0;
 }
 
